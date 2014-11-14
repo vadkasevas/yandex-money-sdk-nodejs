b6f6c2bd1ef12d74f525b582b41518f84e8de7ce
Your Name <Rayman.k26@gmail.com>
2014-11-14 17:51:33 +0300

    hosts configuration

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var wallet = require("./lib/wallet.js");
 var externalPayment = require("./lib/external_payment.js");
+var base = require("./lib/base.js");
 
 module.exports = {
   Wallet: wallet.Wallet,
-  ExternalPayment: externalPayment.ExternalPayment
+  ExternalPayment: externalPayment.ExternalPayment,
+  Config: base.Config
 };
